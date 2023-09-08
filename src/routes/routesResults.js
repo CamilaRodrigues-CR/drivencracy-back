2aaba5b4c1c26ba9444d9a418ef07176ca71c9e5
Driven Education <camilarodriguescrs13@gmail.com>
2023-09-08 14:53:11 -0300

    configurações para deploy

---
+++ src/routes/routesResults.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getResults } from "../controllers/controllersResults.js";
+
+
+const routerResult = Router()
+
+routerResult.get('/poll/:id/result', getResults)
+
+export default routerResult
